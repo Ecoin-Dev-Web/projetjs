53bb8f17b6663d5d89edea1df241acb544bfc0bf
glizlydia <glizlydia886@gmail.com>
2023-04-08 12:18:50 +0100

    learning arrays

--- pr.js
+++ pr.js
@@ -1,1 +1,41 @@
-console.log('lydia');
+var alaa=25
+var ana=16.66
+var car=5
+var hi=22
+
+console.log(typeof alaa)
+console.log( alaa)
+alert(ana)
+console.log(hi)
+
+var isadmin=true
+console.log(typeof isadmin)
+var listNote1=[15,19,12,20,3] 
+var listeNote4 =[] 
+  listeNote4[0]= 20
+  listeNote4[1]= 15
+  listeNote4[2]= 32
+  listeNote4[3]= 20
+  listeNote4[4]= 20
+
+var listeNote2 =new Array(15,19,12,5254,8)
+var listNom =new Array() 
+listNom[0]="alaa"
+listNom[1]="lydia"
+listNom[2]="ahmed"
+
+ console.log(listNom)
+
+ var imagelist=["img/001.png","img/002.png","img/003.png"]
+
+console.log(imagelist[2])
+
+var schoolinfo=[ 
+
+                    ["021203252","023102526"],
+                     ["081203252","056845926"],
+                     ["071203252","023102526"]
+
+]
+
+console.log(schoolinfo[2][1])
